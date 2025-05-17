dc924be77d1691ccdcd1f7f91f4ab8d47f17efac
SriHaran23 <sriharankandepalli@gmail.com>
2025-05-17 12:58:33 +0530

    quiz commit

---
+++ quiz.js
@@ -0,0 +1,112 @@
+var questions= [
+    {
+        "no": 1,
+        "ques": "Which of the following is the correct way to add an event listener in JavaScript?",
+        "options": [
+            "element.onClick = function() {}",
+            "element.addEventListener('click', function() {})",
+            "addEventListener(element, 'click', function() {})",
+            "element.addEventListener('onClick', function() {})"
+        ],
+        "ans": "element.addEventListener('click', function() {})"
+    },
+    {
+        "no": 2,
+        "ques": "Which event is triggered when the user clicks on an HTML element?",
+        "options": [
+            "onSubmit",
+            "onChange",
+            "onClick",
+            "onLoad"
+        ],
+        "ans": "onClick"
+    },
+    {
+        "no": 3,
+        "ques": "What does the preventDefault() method do in an event handler?",
+        "options": [
+            "It prevents the default action associated with the event.",
+            "It prevents the event from propagating.",
+            "It prevents the event from being logged to the console.",
+            "It stops the event from triggering."
+        ],
+        "ans": "It prevents the default action associated with the event."
+    },
+    {
+        "no": 4,
+        "ques": "Which event is fired when the user presses a key on the keyboard?",
+        "options": [
+            "onClick",
+            "onChange",
+            "onKeyDown",
+            "onSubmit"
+        ],
+        "ans": "onKeyDown"
+    },
+    {
+        "no": 5,
+        "ques": "Which of the following is NOT a valid event type for the addEventListener method?",
+        "options": [
+            "mouseover",
+            "keydown",
+            "focus",
+            "clickIn"
+        ],
+        "ans": "clickIn"
+    },
+    {
+        "no": 6,
+        "ques": "What will happen if you use the stopPropagation() method inside an event listener?",
+        "options": [
+            "The event will stop triggering.",
+            "The event will stop propagating up or down the DOM hierarchy.",
+            "The event will execute in the opposite direction.",
+            "The event handler will be removed from the element."
+        ],
+        "ans": "The event will stop propagating up or down the DOM hierarchy."
+    },
+    {
+        "no": 7,
+        "ques": "Which of the following events occurs when the user hovers over an HTML element?",
+        "options": [
+            "onFocus",
+            "onMouseOver",
+            "onChange",
+            "onSubmit"
+        ],
+        "ans": "onMouseOver"
+    },
+    {
+        "no": 8,
+        "ques": "Which method is used to remove an event listener from an element?",
+        "options": [
+            "element.removeListener()",
+            "element.removeEventListener()",
+            "element.deleteEventListener()",
+            "element.remove()"
+        ],
+        "ans": "element.removeEventListener()"
+    },
+    {
+        "no": 9,
+        "ques": "Which event is triggered when the focus is moved to an element?",
+        "options": [
+            "onFocus",
+            "onBlur",
+            "onChange",
+            "onHover"
+        ],
+        "ans": "onFocus"
+    },
+    {
+        "no": 10,
+        "ques": "What does the this keyword represent inside an event handler?",
+        "options": [
+            "It refers to the event itself.",
+            "It refers to the target element that fired the event.",
+            "It refers to the document object.",
+            "It refers to the global object (window)."
+        ],
+        "ans": "It refers to the target element that fired the event."
+    }
+]
